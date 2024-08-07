10121541ccd86f05a0abb792b559833232a4fd9d
Ewan Nisbet <ewan-git@thatdogmachine.com>
2024-08-07 03:49:09 +0100

    docs: cleanup output & add comment regarding flutter 24

--- shell.nix
+++ shell.nix
@@ -7,16 +7,17 @@ let
 in
 pkgs.mkShellNoCC {
   buildInputs = with pkgs;[
-    flutterPackages.v3_22  
+    flutterPackages.v3_22
+    # flutterPackages.v3_24 - _23 is currently latest available
   ];
   shellHook = ''
     # make sure `pub global` commands are reachable:
     export PATH="$PATH":"$HOME/.pub-cache/bin"
-    echo ""
-    echo "Activating serverpod globally..."
+    # echo ""
+    # echo "Activating serverpod globally..."
     # dart pub global activate serverpod_cli 2.0.2
-    echo ""
-    echo "Activating jaspr globally..."
+    # echo ""
+    # echo "Activating jaspr globally..."
     # dart pub global activate jaspr_cli 0.13.3
     echo ""
     ${showDartVersion}
