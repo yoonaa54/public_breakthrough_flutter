eae67d5a40107b639770d4ea6eb51829e9f1a75e
Ewan Nisbet <ewan-git@thatdogmachine.com>
2024-08-10 09:28:49 +0100

    docs: cache for assets needs manual hot reload to clear

--- lib/functions.dart
+++ lib/functions.dart
@@ -1,8 +1,15 @@
-import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show PlatformAssetBundle;
+// import 'package:flutter/services.dart' show rootBundle;
 
-Future<String> readMarkdownFromAssets(String filename) async {
+Future<String> readMarkdownFromAssets(
+    BuildContext context, String filename) async {
   try {
-    String contents = await rootBundle.loadString(filename);
+    final assetBundle = PlatformAssetBundle();
+    // String contents = await rootBundle.loadString(filename, cache: false);
+    String contents = await assetBundle.loadString(filename, cache: false);
+    // cache faluse doesn't appear to work, need to trigger hot reload to work
+    // assetBundle.clear();
     return contents;
   } catch (e) {
     print("Error reading file in readMarkdownFromAssets: $e");
