2de2d3d1b9e6de2a8a3dd2e1d661bc7f507b65a7
Ewan Nisbet <ewan-git@thatdogmachine.com>
2024-08-23 01:41:26 +0100

    feat switch to universal_html

--- lib/calendar_booking/calendly.dart
+++ lib/calendar_booking/calendly.dart
@@ -2,7 +2,7 @@
 import 'package:js/js.dart';
 import 'package:flutter/material.dart';
 import 'dart:ui_web' as ui;
-import 'dart:html' as html;
+import 'package:universal_html/html.dart' as html;
 
 class CalendlyPage extends StatelessWidget {
   const CalendlyPage({super.key});
@@ -20,62 +20,8 @@ class CalendlyPage extends StatelessWidget {
   }
 }
 
-// class CalendlyWidget extends StatefulWidget {
-//   const CalendlyWidget({super.key});
-
-//   @override
-//   CalendlyWidgetState createState() => CalendlyWidgetState();
-// }
-
-// class CalendlyWidgetState extends State<CalendlyWidget> {
-//   @override
-//   void initState() {
-//     super.initState();
-//     // Trigger the Calendly widget initialization after the element is rendered
-//     Future.delayed(Duration.zero, () {
-//       _initializeCalendlyWidget();
-//     });
-//   }
-
-// class CalendlyWidgetState extends State<CalendlyWidget> {
-//   @override
-//   void initState() {
-//     super.initState();
-//     // Trigger the Calendly widget initialization after the element is rendered
-//     Future.delayed(Duration.zero, () {
-//       initCalendlyWidget(); // Call the JS function directly
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return HtmlElementView(viewType: 'calendly-html');
-//   }
-
 @JS('initCalendlyWidget')
 external void initCalendlyWidget();
-
-// void _initializeCalendlyWidget() {
-//   const script = '''
-//     initCalendlyWidget();
-//   ''';
-//   // Execute the script in the browser's context
-//   // ui.window.eval(script);
-//   // (context as Element).mountElement as HtmlElementView
-//   //   ..webOnlyWindowManager.window.eval(script);
-//   if (html.window != null) {
-//     // html.window.eval(script);
-//     context.callMethod('eval', [script]);
-//   }
-//   // context.callMethod('eval', [script]);
-//   // window.eval(script);
-// }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const HtmlElementView(viewType: 'calendly-html');
-//   }
-// }
 
 class CalendlyWidget extends StatefulWidget {
   const CalendlyWidget({super.key});
